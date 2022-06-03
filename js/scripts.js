877094a95a1e8a92617052a983559de4bc34885d
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-03 15:36:21 -0700

    WIP:Trying to hide pervious answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 $(document).ready(function () {
   $("form#survey").submit(function (event) {
+    $("#result").click(function () {
+      $(this).remove();
+    });
     event.preventDefault();
-    const q1Input = $("input:radio[name=knowledge]:checked").val();
-    const q1 = parseInt(q1Input);
-    const q2Input = $("input:radio[name=platform]:checked").val();
-    const q2 = parseInt(q2Input);
-    const q3Input = $("input:radio[name=companySize]:checked").val();
-    const q3 = parseInt(q3Input);
-    const q4Input = $("input:radio[name=reason]:checked").val();
-    const q4 = parseInt(q4Input);
-    const q5Input = $("input:radio[name=difficulty]:checked").val();
-    const q5 = parseInt(q5Input);
+    const q1 = parseInt($("input:radio[name=knowledge]:checked").val());
+    const q2 = parseInt($("input:radio[name=platform]:checked").val());
+    const q3 = parseInt($("input:radio[name=companySize]:checked").val());
+    const q4 = parseInt($("input:radio[name=reason]:checked").val());
+    const q5 = parseInt($("input:radio[name=difficulty]:checked").val());
     let result = q1 + q2 + q3 + q4 + q5;
     console.log(result);
-
     if (result >= 5 && result < 10) {
       $("#javaScript").show();
     } else if (result >= 10 && result < 15) {
