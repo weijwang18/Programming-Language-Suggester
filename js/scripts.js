5f9cb175ec4b7c58658005d4931d06293eadf6aa
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-03 14:25:32 -0700

    WIP: Working on logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
   $("form#survey").submit(function (event) {
     event.preventDefault();
-    const q1Input = $("input:radio[name=knowledge]:checked").val();
-    const q2Input = $("input:radio[name=platform]:checked").val();
-    const q3Input = $("input:radio[name=companySize]:checked").val();
-    const q4Input = $("input:radio[name=reason]:checked").val();
-    const q5Input = $("input:radio[name=microsoft]:checked").val();
+    const q1Input = $parseInt("input:radio[name=knowledge]:checked").val();
+    const q2Input = $parseInt("input:radio[name=platform]:checked").val();
+    const q3Input = $parseInt("input:radio[name=companySize]:checked").val();
+    const q4Input = $parseInt("input:radio[name=reason]:checked").val();
+    const q5Input = $parseInt("input:radio[name=difficulty]:checked").val();
+    const result = q1Input + q2Input + q3Input + q4Input + q5Input;
 
-    if (q2Input === "Web") {
-      $("#javaScript").show();
-    }
-  });
+  if ( result < 15 ) {
+      $("#javascript").show();
+    } else if (result ) {
+      $("#java").show();
+    } else if (result ) {
+      $("#cSharp").show();
+    } else {$("#cPlus").show();}
+  
 });
