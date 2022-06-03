2ce1da6d8a41a207fc9a51c485c76e6e3ad45583
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-03 14:46:24 -0700

    restructure the logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
   $("form#survey").submit(function (event) {
     event.preventDefault();
-    const q1Input = $parseInt("input:radio[name=knowledge]:checked").val();
-    const q2Input = $parseInt("input:radio[name=platform]:checked").val();
-    const q3Input = $parseInt("input:radio[name=companySize]:checked").val();
-    const q4Input = $parseInt("input:radio[name=reason]:checked").val();
-    const q5Input = $parseInt("input:radio[name=difficulty]:checked").val();
-    const result = q1Input + q2Input + q3Input + q4Input + q5Input;
+    const q1Input = $("input:radio[name=knowledge]:checked").val();
+    const q1 = parseInt(q1Input);
+    const q2Input = $("input:radio[name=platform]:checked").val();
+    const q2 = parseInt(q2Input);
+    const q3Input = $("input:radio[name=companySize]:checked").val();
+    const q3 = parseInt(q3Input);
+    const q4Input = $("input:radio[name=reason]:checked").val();
+    const q4 = parseInt(q4Input);
+    const q5Input = $("input:radio[name=difficulty]:checked").val();
+    const q5 = parseInt(q5Input);
+    let result = q1 + q2 + q3 + q4 + q5;
+    console.log(result);
 
-  if ( result < 15 ) {
-      $("#javascript").show();
-    } else if (result ) {
+    if (result >= 5 && result < 10) {
+      $("#javaScript").show();
+    } else if (result >= 10 && result < 15) {
       $("#java").show();
-    } else if (result ) {
-      $("#cSharp").show();
-    } else {$("#cPlus").show();}
-  
+    } else {
+      $("#cPlus").show();
+    }
+  });
 });
